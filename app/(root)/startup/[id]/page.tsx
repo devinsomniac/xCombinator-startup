1af4c747751ca039ca2f6f766e76e0240fd39ba2
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-07 23:51:03 +0600

    Linked button with to startup up

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import posts from '@/posts'
+import Image from 'next/image'
 const page = () => {
   return (
     <div>
-        Hello World
+        <section className='yellow_container'>
+            <p className='tag'>{posts[1].createData}</p>
+            <h2 className='heading'>{posts[1].start_title}</h2>
+            <p className='sub-heading'>{posts[1].start_desc}</p>
+        </section>
+        <section className='p-8'>
+            <div className='p-5 flex items-center gap-2'>
+                <Image src={posts[1].user_image} alt='user_photo' width={45} height={45}/>
+                <p>{posts[1].user_name}</p>
+            </div>
+        </section>
     </div>
   )
 }
