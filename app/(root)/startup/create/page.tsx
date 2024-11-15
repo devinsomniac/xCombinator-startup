565754d000be3e3bf0db0e6c4ee5c8e070ea913b
Inzamam <inzamamchowdhury20@gmail.com>
2024-11-16 00:35:59 +0600

    Changed the function name

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -9,42 +9,44 @@ import { db } from '@/Database/db'
 import { startups } from '@/Database/schema'
 import { useSession } from 'next-auth/react'
 
-const page = () => {
-  const {data:session} = useSession()
+const Page = () => {
+  const { data: session } = useSession()
   console.log(session)
   const router = useRouter()
   const [pending, setPending] = useState(false)
-  const [formData,setFormData] = useState({
-    title:'',
-    description:'',
-    category:'',
-    imageLink:'',
-    pitch:''
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    category: '',
+    imageLink: '',
+    pitch: ''
   })
 
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {name,value} = e.target
-    setFormData((prev) => (
-      {
-        ...prev,[name]:value
-      }
-    ))
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value
+    }))
   }
 
-  const handleSubmit = async(e : React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     console.log(formData)
     setPending(true)
-    try{
+    try {
       await db.insert(startups).values({
         ...formData,
-        userId:session?.user?.id as string
+        userId: session?.user?.id as string
       })
       router.push('/')
-    }catch(err){
-      console.log("Error in submitting data",err)
+    } catch (err) {
+      console.log("Error in submitting data", err)
+    } finally {
+      setPending(false)
     }
   }
+
   return (
     <div>
       <section className='yellow_container'>
@@ -52,52 +54,58 @@ const page = () => {
         <p className='sub-heading'>Have an innovative concept? We are here to help bring it to life. Submit your idea and join us in building the next big thing.</p>
       </section>
       <section className='create_section'>
-        <form >
+        <form>
           <div className='grid grid-cols-1 md:grid-cols-2'>
-          <label className='create_section_label'>Title</label>
-          <Input
-            className='mt-2 h-12 rounded-full border-black border-[3px]'
-            name='title'
-            value={formData.title}
-            onChange={handleChange}
-          />
-          <label className='create_section_label'>Description</label>
-          <Textarea
-            className='mt-2 h-12 rounded-xl border-black border-[3px]'
-            name='description'
-            value={formData.description}
-            onChange={handleChange}
-          />
-          <label className='create_section_label'>Category</label>
-          <Input
-            className='mt-2 h-12 rounded-full border-black border-[3px]'
-            name='category'
-            value={formData.category}
-            onChange={handleChange}
-          />
-          <label className='create_section_label'>Image Link</label>
-          <Input
-            className='mt-2 h-12 rounded-full border-black border-[3px]'
-            name='imageLink'
-            value={formData.imageLink}
-            onChange={handleChange}
-          />
-          <label className='create_section_label'>Pitch</label>
-          <Textarea
-            className='mt-2 h-12 rounded-xl border-black border-[3px]'
-            name='pitch'
-            value={formData.pitch}
-            onChange={handleChange}
-          />
+            <label className='create_section_label'>Title</label>
+            <Input
+              className='mt-2 h-12 rounded-full border-black border-[3px]'
+              name='title'
+              value={formData.title}
+              onChange={handleChange}
+            />
+            <label className='create_section_label'>Description</label>
+            <Textarea
+              className='mt-2 h-12 rounded-xl border-black border-[3px]'
+              name='description'
+              value={formData.description}
+              onChange={handleChange}
+            />
+            <label className='create_section_label'>Category</label>
+            <Input
+              className='mt-2 h-12 rounded-full border-black border-[3px]'
+              name='category'
+              value={formData.category}
+              onChange={handleChange}
+            />
+            <label className='create_section_label'>Image Link</label>
+            <Input
+              className='mt-2 h-12 rounded-full border-black border-[3px]'
+              name='imageLink'
+              value={formData.imageLink}
+              onChange={handleChange}
+            />
+            <label className='create_section_label'>Pitch</label>
+            <Textarea
+              className='mt-2 h-12 rounded-xl border-black border-[3px]'
+              name='pitch'
+              value={formData.pitch}
+              onChange={handleChange}
+            />
           </div>
           <div className='flex justify-end'>
-          <Button type='submit' disabled={pending} className='bg-[#f4d301] mt-4 h-10 rounded-full border-[3px] border-black' onClick={handleSubmit}>{pending ? "Submitting..." : "Submit"}</Button>
+            <Button
+              type='submit'
+              disabled={pending}
+              className='bg-[#f4d301] mt-4 h-10 rounded-full border-[3px] border-black'
+              onClick={handleSubmit}
+            >
+              {pending ? "Submitting..." : "Submit"}
+            </Button>
           </div>
         </form>
-
       </section>
     </div>
   )
 }
 
-export default page
+export default Page
